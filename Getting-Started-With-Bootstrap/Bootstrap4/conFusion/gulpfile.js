ab82d62c05bdd6172a9023d4a2991fd26aa9864d
David Kim <youup99@gmail.com>
2020-06-02 16:30:46 -0600

    Gulp Part 2

--- Getting-Started-With-Bootstrap/Bootstrap4/conFusion/gulpfile.js
+++ Getting-Started-With-Bootstrap/Bootstrap4/conFusion/gulpfile.js
@@ -2,6 +2,14 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin'),
     browserSync = require('browser-sync');
 
 
@@ -32,6 +40,43 @@ gulp.task('browser-sync', function() {
 });
 
 // Default task
-gulp.task('default', ['browser-sync'], function() {
-    gulp.start('sass:watch');
+gulp.task('default', gulp.series('browser-sync', 'sass:watch'));
+
+// Clean
+gulp.task('clean', function() {
+    return del(['dist']);
 });
+
+// Images
+gulp.task('imagemin', function() {
+    return gulp.src('img/*.{png,jpg,gif}')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('copyfonts', function() {
+
+    return gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+
+    .pipe(gulp.dest('./dist/fonts'));
+
+});
+
+gulp.task('usemin', function() {
+    return gulp.src('./*.html')
+        .pipe(flatmap(function(stream, file) {
+            return stream
+                .pipe(usemin({
+                    css: [rev()],
+                    html: [function() { return htmlmin({ collapseWhitespace: true }) }],
+                    js: [uglify(), rev()],
+                    inlinejs: [uglify()],
+                    inlinecss: [cleanCss(), 'concat']
+                }))
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', gulp.series('clean', 'copyfonts', 'imagemin', 'usemin', function(done) { done(); }
+
+));
