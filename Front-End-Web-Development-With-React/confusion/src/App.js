bbf852c7fff4c36acc07607fbb12fe26a7309fd0
David Kim <youup99@gmail.com>
2020-06-07 20:49:15 -0600

    Components Part 1

--- Front-End-Web-Development-With-React/confusion/src/App.js
+++ Front-End-Web-Development-With-React/confusion/src/App.js
@@ -1,5 +1,6 @@
 import { Navbar, NavbarBrand } from "reactstrap";
 import React, { Component } from "react";
+import Menu from './components/MenuComponent';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
             <NavbarBrand href="/"> Ristorante Con Fusion </NavbarBrand>{" "}
           </div>{" "}
         </Navbar>{" "}
+        <Menu/>
       </div>
     );
   }
