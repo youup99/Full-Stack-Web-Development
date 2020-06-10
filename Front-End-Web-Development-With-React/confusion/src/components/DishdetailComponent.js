4ad82fb148ff210ef11121962b9cc84f631adf2d
David Kim <youup99@gmail.com>
2020-06-09 22:03:27 -0600

    Assignment #1

---
+++ Front-End-Web-Development-With-React/confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+
+class DishDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderDish(dish) {
+    if (dish != null)
+      return (
+        <div className="col-12 col-md-5 m-1">
+          <Card>
+            <CardImg top src={dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    else return <div></div>;
+  }
+
+  renderComments(dish) {
+    if (dish != null) {
+      const comments = dish.comments.map((comment) => {
+        return (
+          <div>
+            <p>{comment.comment}</p>
+            <p>
+              -- {comment.author} ,{" "}
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+
+                month: "long",
+
+                day: "2-digit",
+              }).format(new Date(comment.date))}
+            </p>
+          </div>
+        );
+      });
+      return (
+        <div className="col-12 col-md-5 m-1">
+          <h4>Comments</h4>
+          <ul className="list-unstyled">{comments}</ul>
+        </div>
+      );
+    } else return <div></div>;
+  }
+
+  render() {
+    return (
+      <div className="row">
+        {this.renderDish(this.props.dish)}
+        {this.renderComments(this.props.dish)}
+      </div>
+    );
+  }
+}
+
+export default DishDetail;
