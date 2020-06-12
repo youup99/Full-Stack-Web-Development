b34ab46bd15386e39a426d8ef9811e6175b9343e
David Kim <youup99@gmail.com>
2020-06-11 23:16:05 -0600

    Presentational and Container Components

--- Front-End-Web-Development-With-React/confusion/src/App.js
+++ Front-End-Web-Development-With-React/confusion/src/App.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
 import Main from './components/MainComponent';
-import { DISHES } from './shared/dishes';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
   render() {
     return (
       <div className="App">
