5b481682e0e954e41ef27f55953b0120995c9d28
David Kim <youup99@gmail.com>
2020-10-17 15:04:22 -0600

    Cookies

--- Server-Side Development with NodeJS, Express, and MongoDB/NodeJS/conFusionServer/app.js
+++ Server-Side Development with NodeJS, Express, and MongoDB/NodeJS/conFusionServer/app.js
@@ -38,29 +38,40 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-function auth(req, res, next) {
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-    var err = new Error("You are not authenticated!");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    next(err);
-    return;
-  }
+app.use(cookieParser("12345-67890-09876-54321"));
 
-  var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-    .toString()
-    .split(":");
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == "admin" && pass == "password") {
-    next(); // authorized
+function auth(req, res, next) {
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+      return;
+    }
+    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == "admin" && pass == "password") {
+      res.cookie("user", "admin", { signed: true });
+      next(); // authorized
+    } else {
+      var err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+    }
   } else {
-    var err = new Error("You are not authenticated!");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    next(err);
+    if (req.signedCookies.user === "admin") {
+      next();
+    } else {
+      var err = new Error("You are not authenticated!");
+      err.status = 401;
+      next(err);
+    }
   }
 }
 
@@ -87,7 +98,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
-
 
 module.exports = app;
