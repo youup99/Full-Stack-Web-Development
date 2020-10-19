3a3afc667d4f9444fa9be4430a83f75eb61f7844
David Kim <youup99@gmail.com>
2020-10-18 20:39:32 -0600

    Passport

--- Server-Side Development with NodeJS, Express, and MongoDB/NodeJS/conFusionServer/app.js
+++ Server-Side Development with NodeJS, Express, and MongoDB/NodeJS/conFusionServer/app.js
@@ -4,6 +4,9 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+var passport = require("passport");
+var authenticate = require("./authenticate");
+
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
 
@@ -51,24 +54,21 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error("You are not authenticated!");
     err.status = 403;
-    return next(err);
+    next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      next();
-    } else {
-      var err = new Error("You are not authenticated!");
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 
